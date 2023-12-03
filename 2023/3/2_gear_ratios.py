5205c8c25c86cc1877b7116d06c3f9bce8bb50fa
JSpeleers <jasper_speleers@msn.com>
2023-12-03 20:20:34 +0100

    2023/3 part 2

--- 2023/3/2_gear_ratios.py
+++ 2023/3/2_gear_ratios.py
@@ -4,18 +4,21 @@ def run_part2(filename):
     count = 0
     for i in range(len(text)):
         print()
-        print(text[i])
         for j in range(len(text[i])):
             c = text[i][j]
             if _is_gear(c):
+                if i - 1 >= 0: print(text[i - 1])
+                print(text[i])
+                if i + 1 < len(text): print(text[i + 1])
                 adj_coords = _get_adjacent_coords([(i, j)])
                 print(f'Adj Coords: {adj_coords}')
                 print(f'Adj Text: {_coords_to_text(text, adj_coords)}')
                 adj_numbers = _get_adjacent_numbers(text, adj_coords)
                 print(f'Adj Numbers: {adj_numbers}')
                 if len(adj_numbers) == 2:
-                    count += adj_numbers.pop() * adj_numbers.pop()
-                # exit()
+                    to_add = (adj_numbers.pop() * adj_numbers.pop())
+                    count += to_add
+                    print(f'Adding {to_add} new count {count}')
     return count
 
 
@@ -39,8 +42,8 @@ def _get_adjacent_coords(coords):
 
 def _get_adjacent_numbers(text, coords):
     # TODO filter out duplicates without using a set
-    return set(_get_full_number(text, coord[0], coord[1]) for coord in coords if
-            _try_get_text(text, coord[0], coord[1]).isdigit())
+    return [x[0] for x in set(_get_full_number(text, coord[0], coord[1]) for coord in coords if
+                              _try_get_text(text, coord[0], coord[1]).isdigit())]
 
 
 def _get_full_number(text, x, y):
@@ -51,13 +54,11 @@ def _get_full_number(text, x, y):
         c = text[x][y - a: y + b]
         a += 1
     a = max(0, a - 1)
-    # a = a - 1 if y - a < 0 else a
-    while text[x][y - a: y + b].isdigit() and y + b < len(text[x]):
+    while text[x][y - a: y + b].isdigit() and y + b <= len(text[x]):
         c = text[x][y - a: y + b]
         b += 1
-    # b = b - 1 if not y + b < len(text[x]) else b
-    print(f'FULL: {c} -- {a} vs {b}')
-    return int(c)
+    b = b - 1 if not y + b < len(text[x]) else b
+    return int(c), y - a, y + b
 
 
 def _coords_to_text(text, coords):
@@ -66,11 +67,15 @@ def _coords_to_text(text, coords):
 
 def _try_get_text(text, x, y):
     try:
-        return text[x][y]
+        return '' if x < 0 or y < 0 else text[x][y]
     except IndexError:
         return ''
 
 
 if __name__ == "__main__":
-    print(f'Result = {run_part2("example_1.txt")}')
-    # print(f'Result = {run_part2("input.txt")}')
+    # example = run_part2("example_2.txt")
+    # print(f'Should be 467835')
+    input = run_part2("input.txt")
+
+    # print(f'Example = {example}')
+    print(f'Input = {input}')
