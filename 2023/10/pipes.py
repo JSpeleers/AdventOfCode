965d08f23f54f1bc96839d803804a5958702ba70
JSpeleers <jasper_speleers@msn.com>
2023-12-10 14:06:56 +0100

    2023/10 p2 WIP

--- 2023/10/pipes.py
+++ 2023/10/pipes.py
@@ -41,10 +41,13 @@ class Pipe:
         self.y = y
 
     def __repr__(self):
-        return self.char
+        return f'{self.char}({self.x},{self.y})'
 
     def get_all_adj_coords(self):
         return [self.get_adj_coords(orientation) for orientation in orientations if self.has_adj(orientation)]
+
+    def get_all_coords(self):
+        return [self.get_adj_coords(orientation) for orientation in orientations]
 
     def get_adj_coords(self, orientation):
         if orientation == 'top':
@@ -90,9 +93,91 @@ def _count_path(pipe: Pipe, orientation, length=0):
 
 def _find_pipe(x, y) -> Pipe | None:
     try:
+        if x < 0 or y < 0:
+            return None
         return pipes[x][y]
     except IndexError:
         return None
+
+
+@aoc_solution(2023, 10, 2)
+def run_part2(filename):
+    print()
+    start = _read_input(filename)
+    path = []
+    for orientation in orientations:
+        path = _build_path(start, orientation, set())
+        if path is not None and len(path) > 0:
+            break
+    sections = _build_sections(path)
+    for section in sections:
+        print(f'{len(section)} = {section}')
+    return _count_inside_sections(sections, path)
+
+
+def _build_path(pipe: Pipe, orientation, path):
+    if pipe.char == 'S' and len(path) != 0:
+        return path
+    if pipe.has_adj(orientation):
+        # print(f'pipe {pipe} has {orientation}')
+        adj_x, adj_y = pipe.get_adj_coords(orientation)
+        adj_pipe = _find_pipe(adj_x, adj_y)
+        if adj_pipe is not None and adj_pipe.has_adj(reverse_of[orientation]):
+            # print(f'{adj_pipe} and next is {adj_pipe.next_orientation(reverse_of[orientation])}')
+            path.add(pipe)
+            return _build_path(adj_pipe, adj_pipe.next_orientation(reverse_of[orientation]), path)
+    return None
+
+
+def _build_sections(path):  # Flood fill
+    section_list = []
+    traversed_pipes = set()
+    for pipeline in pipes:
+        for pipe in pipeline:
+            if pipe not in traversed_pipes and pipe not in path:
+                found_section = _build_section(pipe, set(), path)
+                section_list.append(found_section)
+                traversed_pipes = traversed_pipes.union(found_section)
+    return section_list
+
+
+def _build_section(current_pipe: Pipe, traversed_pipes, path):
+    # print(f'Building section for {current_pipe}')
+    if current_pipe in traversed_pipes or current_pipe in path:
+        return traversed_pipes
+    else:
+        traversed_pipes.add(current_pipe)
+        for coord in current_pipe.get_all_coords():
+            # print(f'Finding pipe for {coord}')
+            new_pipe = _find_pipe(coord[0], coord[1])
+            if new_pipe is not None:
+                # print(f'From {current_pipe} to {new_pipe}')
+                traversed_pipes = traversed_pipes.union(_build_section(new_pipe, traversed_pipes, path))
+    return traversed_pipes
+
+
+def _count_inside_sections(sections, path):
+    count = 0
+    for section in sections:
+        current_pipe = section.pop()
+        if current_pipe.x == 0 or current_pipe.y == 0:
+            continue
+        else:
+            lefties = _count_number_of_path_to_the_left(current_pipe, path)
+            # print(f'Counting liefties for {current_pipe} = {lefties}')
+            if lefties > 0 and lefties % 2 != 0:
+                # print(f'Found inside section {section}')
+                count += len(section) + 1
+    return count
+
+
+def _count_number_of_path_to_the_left(pipe: Pipe, path):
+    count = 0
+    y = pipe.y - 1
+    while y >= 0:
+        count = count + 1 if pipes[pipe.x][y] in path else count
+        y -= 1
+    return count
 
 
 if __name__ == "__main__":
@@ -100,3 +185,8 @@ if __name__ == "__main__":
     run_part1("example_1.txt")  # 4
     run_part1("example_2.txt")  # 8
     run_part1("input.txt")
+    run_part2("example_3.1.txt")  # 4
+    run_part2("example_3.2.txt")  # 4
+    run_part2("example_4.txt")  # 8
+    # run_part2("example_5.txt")  # 10
+    # run_part2("input.txt")
