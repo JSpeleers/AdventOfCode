c5574df3e2f045e4cfadb793f0658730f356597c
JSpeleers <jasper_speleers@msn.com>
2023-12-13 01:34:53 +0100

    2023/12 p2 WIP

--- 2023/12/lavacombinations.py
+++ 2023/12/lavacombinations.py
@@ -1,33 +1,25 @@
 from util.decorators import aoc_timed_solution
 
 
-def _read_input(filename):
-    with open(filename) as file:
-        text = []
-        for i, line in enumerate(file):
-            text.append([])
-            for c in line.rstrip():
-                text[i].append(c)
-    return text
-
-
 @aoc_timed_solution(2023, 12, 1)
-def run_part1(filename, new=False):
+def run_part1(filename, new=False): # Brute force
     count = 0
     with open(filename) as file:
         for line in file:
             this_count = count_possible_combinations(line.split()[0], [int(i) for i in line.split()[1].split(',')], new)
-            print(f'{this_count} combinations for {line.split()[0]} - {[int(i) for i in line.split()[1].split(",")]}')
+            # print(f'{this_count} combinations for {line.split()[0]} - {[int(i) for i in line.split()[1].split(",")]}')
             count += this_count
     return count
 
 
 def count_possible_combinations(record, groups, new=False):
+    expected_amount = sum(groups)
     combinations = _generate_combinations(record)
+    print(f'Got {len(combinations)} combinations')
     count = 0
     if new:
         for combination in combinations:
-            count += 1 if _is_equal_to(groups, combination) else 0
+            count += 1 if _is_equal_to(groups, expected_amount, combination) else 0
 
     else:
         for combination in combinations:
@@ -36,62 +28,49 @@ def count_possible_combinations(record, groups, new=False):
 
 
 def _generate_combinations(record):
-    return [
-        record.replace('?', '{}').format(*bin(i)[2:].zfill(record.count('?')).replace('0', '.').replace('1', '#'))
-        for i in range(2 ** record.count('?'))]
+    qs = record.count('?')
+    r = record.replace('?', '{}')
+    return [r.format(*bin(i)[2:].zfill(qs).replace('0', '.').replace('1', '#')) for i in range(2 ** qs)]
 
 
 def _get_groups_of_combination(combination):
     return [len(x) for x in combination.split('.') if x != '']
 
 
-def _is_equal_to(expected, combination):
+def _is_equal_to(expected, expected_amount, combination):
+    if expected_amount != combination.count('#'):
+        return False
+
     current_group_length = 0
     index = 0
 
-    print(f'\nChecking if {combination} groups is {expected}')
-
     try:
         for char in combination:
-            print(f'Got {char} ({current_group_length})')
             if char == '#':
                 current_group_length += 1
-                print('Adding length')
             elif current_group_length > 0:
                 if expected[index] != current_group_length:
-                    print(f'FALSE - Got group {current_group_length} and that is not equal to index {index} = {expected[index]}')
                     return False
                 else:
-                    print(f'Equal, continuing')
                     current_group_length = 0
                     index += 1
-
-
-
-        print(f'Ended loop on ({current_group_length}) and {index} and {len(expected)}')
         if current_group_length == 0 and index == len(expected):
-            print(f'TRUE - group is 0 at the end')
             return True
         elif expected[index] != current_group_length and index + 1 != len(expected):
-            print(f'FALSE - Got group {current_group_length} and that is not equal to {index} = {expected[index]} (end)')
             return False
-
     except IndexError:
-        print(f'FALSE IndexErr')
         return False
-
-    print(f'TRUE - loop done')
     return True
 
 
 @aoc_timed_solution(2023, 12, 2)
-def run_part2(filename):
+def run_part2(filename, new=False):
     count = 0
     with open(filename) as file:
         for line in file:
             record = ((line.split()[0] + '?') * 5)[:-1]
             groups = [int(i) for i in line.split()[1].split(',')] * 5
-            this_count = count_possible_combinations(record, groups)
+            this_count = count_possible_combinations(record, groups, new)
             print(f'{this_count} combinations for {record} - {groups}')
             count += this_count
     return count
@@ -100,9 +79,17 @@ def run_part2(filename):
 if __name__ == "__main__":
     # run_part1("example_1.txt")  # 21
     # run_part1("example_1.txt", new=True)  # 21
-    # run_part1("example_1.2.txt")  # 4
-    run_part1("example_1.6.txt")  # 10
-    run_part1("example_1.6.txt", new=True)  # 10
+    # run_part1("example_1.6.txt")  # 10
+    # run_part1("example_1.6.txt", new=True)  # 10
     # run_part1("input.txt")
+    # run_part1("input.txt", new=True)
+    run_part2("example_1.1.txt")  # 1
+    run_part2("example_1.1.txt", new=True)  # 1
+    # run_part2("example_1.2.txt")  # 16384
+    # run_part2("example_1.2.txt", new=True)  # 16384
+    # run_part2("example_1.4.txt")  # 10
+    # run_part2("example_1.4.txt", new=True)  # 10
+    # run_part2("example_1.5.txt") # 2500
+    # run_part2("example_1.5.txt", new=True)  # 2500
     # run_part2("example_1.txt") # 525152
     # run_part2("input.txt")
